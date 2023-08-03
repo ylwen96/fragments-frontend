ab4517d789d2e9a94fb75f846a748363ad416e68
yuelin wen <tyleryuelinwen@gmail.com>
2023-08-03 02:09:35 -0400

    set up page, add mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div>
-      123
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+// export default withAuthenticator(App, {
+//   signUpAttributes: ['email', 'name'],
+// });
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      Home page
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
