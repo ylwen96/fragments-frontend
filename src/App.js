bd0e33d2999ab719cc57c0ee2b087cb4598f03c5
yuelin wen <tyleryuelinwen@gmail.com>
2023-08-23 22:56:38 -0400

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import Navbar from "./components/navbar/navbar";
-import PrivateRoutes from "./util/privateRoutes.js";
+// import PrivateRoutes from "./util/privateRoutes.js";
 import Fragment from "./pages/fragment/Fragment";
-
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const isUserSignedIn = useSelector((state) => state.auth.isUserSignedIn);
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/fragments/:id" element={<Fragment />} />
-        </Route>
+      {isUserSignedIn ? (<Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/fragments/:id" element={<Fragment />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      </Routes>) : (<Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>)}
+
     </>)
 }
 
